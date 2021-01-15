bc98c3ca024a59330f2919212a0aa5d5b09e9353
SweetGamerPro <sweetgamerpro140@gmail.com>
2021-01-15 09:55:52 -0300

    Alterado palavras do workspace

--- Calculadora Simples em lua/calc.lua
+++ Calculadora Simples em lua/calc.lua
@@ -7,7 +7,7 @@ local a = io.read("n")
 print('Digite o segundo valor: ')
 local b = io.read("n")
 
-print('Informe a operaçao: ')
+print('Informe a operacao: ')
 print('1 => +\n2 => -\n3 => x\n 4 => /')
 local op = io.read("n")
 
